bb6899ee11727c73e1ac6530ea13f5db9299fd8c
Hosamane K N, Vinay <vhosb@allstate.com>
2017-08-10 10:16:43 +0530

    Added Cancel note

--- screens/NoteDetail.js
+++ screens/NoteDetail.js
@@ -5,10 +5,12 @@ import {
   View,
   TouchableHighlight,
   Alert,
-  TextInput
+  TextInput,
+  ScrollView
 } from "react-native";
 
 import fetchNotes from "../components/fetchNotes";
+import Utils from "../components/Utils";
 
 export default class NoteDetail extends Component {
   constructor(props) {
@@ -18,16 +20,29 @@ export default class NoteDetail extends Component {
       currentNoteTitle: this.props.navigation.state.params.note[0],
       currentNoteDescription: this.props.navigation.state.params.note[1],
       editNote: false,
-      editedTitle:this.props.navigation.state.params.note[0],
-      editedDescription:this.props.navigation.state.params.note[1]
+      editedTitle: this.props.navigation.state.params.note[0],
+      editedDescription: this.props.navigation.state.params.note[1],
+      editTextValue : 'EditNote'
     };
   }
 
   editPress() {
     console.log("Edit pressed");
-    this.setState({
-      editNote: true
-    });
+
+    if (this.state.editTextValue=='EditNote') {
+      this.setState({
+        editNote: true,
+        editTextValue : 'Cancel Edit'
+      });
+    }
+    else {
+      this.setState({
+        editNote: false,
+        editTextValue : 'EditNote'
+      });
+    }
+
+    this.refs.title.focus();
   }
 
   savePress() {
@@ -43,13 +58,36 @@ export default class NoteDetail extends Component {
       {
         text: "OK",
         onPress: () => {
-          fetchNotes.EditNote({oldkey:this.state.currentNoteTitle,newkey:this.state.editedTitle,value:this.state.editedDescription},function(response){
-            if(response)
-            {
-              that.props.navigation.state.params.updateList();
-              that.props.navigation.goBack();
+          Utils.validateNote(
+            this.state.currentNoteTitle,
+            this.state.currentNoteDescription,
+            function(response) {
+              if (response) {
+                fetchNotes.EditNote(
+                  {
+                    oldkey: that.state.currentNoteTitle,
+                    newkey: that.state.editedTitle,
+                    value: that.state.editedDescription
+                  },
+                  function(response) {
+                    if (response) {
+                      that.props.navigation.state.params.updateList();
+                      that.props.navigation.goBack();
+                    }
+                  }
+                );
+              } else {
+                Alert.alert("Note Alert", response.errorMessage, [
+                  {
+                    text: "OK",
+                    onPress: () => {
+                      console.log("error note");
+                    }
+                  }
+                ]);
+              }
             }
-          })
+          );
         }
       }
     ]);
@@ -58,17 +96,31 @@ export default class NoteDetail extends Component {
   deletePress() {
     var that = this;
     console.log("delete pressed");
-    fetchNotes.deleteNote(that.state.currentNoteTitle, function(response) {
-      Alert.alert("Note Alert", "Note is succefully deleted", [
-        {
-          text: "OK",
-          onPress: () => {
-            that.props.navigation.state.params.updateList();
-            that.props.navigation.goBack();
-          }
+
+    Alert.alert("Note Alert", "Do you want to delete this note?", [
+      {
+        text: "OK",
+        onPress: () => {
+          fetchNotes.deleteNote(that.state.currentNoteTitle, function(response) {
+            Alert.alert("Note Alert", "Note is succefully deleted", [
+              {
+                text: "OK",
+                onPress: () => {
+                  that.props.navigation.state.params.updateList();
+                  that.props.navigation.goBack();
+                }
+              }
+            ]);
+          });
         }
-      ]);
-    });
+      },
+      {
+        text:"Cancel",
+        onPress: ()=> {
+
+        }
+      }
+    ]);
   }
 
   render() {
@@ -79,65 +131,68 @@ export default class NoteDetail extends Component {
     var noteDescription = that.props.navigation.state.params.note[1];
 
     return (
-      <View style={styles.container}>
-      <TextInput
-        ref="title"
-        style={styles.title}
-        value ={this.state.editedTitle}
-        multiline={true}
-        editable = {this.state.editNote}
-        returnKeyType={"next"}
-        autoFocus={true}
-        autoCapitalize="none"
-        autoCorrect={false}
-        clearButtonMode="while-editing"
-        onChangeText={text => {
-          this.setState({ editedTitle: text });
-        }}
-        onSubmitEditing={event => {
-          this.refs.description.focus();
-        }}
-      />
-      <TextInput
-        ref="description"
-        style={styles.description}
-        value ={this.state.editedDescription}
-        multiline={true}
-        editable = {this.state.editNote}
-        numberOfLines={10}
-        placeholderTextColor="black"
-        returnKeyType={"done"}
-        autoCapitalize="none"
-        autoCorrect={false}
-        clearButtonMode="while-editing"
-        onChangeText={text => {
-          this.setState({ editedDescription: text });
-        }}
-        onSubmitEditing={event => {}}
-      />
+      <ScrollView>
+        <View style={styles.container}>
+          <TextInput
+            ref="title"
+            style={styles.title}
+            value={this.state.editedTitle}
+            multiline={true}
+            editable={this.state.editNote}
+            returnKeyType={"next"}
+            autoFocus={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            onChangeText={text => {
+              this.setState({ editedTitle: text });
+            }}
+            onSubmitEditing={event => {
+              this.refs.description.focus();
+            }}
+          />
+          <TextInput
+            ref="description"
+            style={styles.description}
+            value={this.state.editedDescription}
+            multiline={true}
+            editable={this.state.editNote}
+            numberOfLines={10}
+            placeholderTextColor="black"
+            returnKeyType={"done"}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            onChangeText={text => {
+              this.setState({ editedDescription: text });
+            }}
+            onSubmitEditing={event => {}}
+          />
 
-        <View style={styles.buttonsView}>
-          {this.state.editNote && <TouchableHighlight
-              onPress={this.savePress.bind(this)}
+          <View style={styles.buttonsView}>
+            {this.state.editNote &&
+              <TouchableHighlight
+                onPress={this.savePress.bind(this)}
+                underlayColor="gray"
+              >
+                <Text style={{ fontSize: 20, color: "blue" }}> SaveNote </Text>
+              </TouchableHighlight>
+            }
+            <TouchableHighlight
+              onPress={this.editPress.bind(this)}
               underlayColor="gray"
             >
-              <Text style={{ fontSize: 20, color: "blue" }}> SaveNote </Text>
+              <Text style={{ fontSize: 20, color: "blue" }}> {this.state.editTextValue} </Text>
             </TouchableHighlight>
-          }
-          <TouchableHighlight
-            onPress={this.editPress.bind(this)}
-            underlayColor="gray"
-          >
-            <Text style={{ fontSize: 20, color: "blue" }}> EditNote </Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={this.deletePress.bind(this)}
-            underlayColor="gray"
-          >
-            <Text style={{ fontSize: 20, color: "blue" }}> Delete Note </Text>
-          </TouchableHighlight>
+            <TouchableHighlight
+              onPress={this.deletePress.bind(this)}
+              underlayColor="gray"
+            >
+              <Text style={{ fontSize: 20, color: "blue" }}> Delete Note </Text>
+            </TouchableHighlight>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -167,12 +222,13 @@ const styles = StyleSheet.create({
     height: 300
   },
   buttonsView: {
-    flex: 0.2,
+    flex: 1,
     flexDirection: "row",
     marginTop: 50,
     marginLeft: 40,
     marginRight: 40,
     alignItems: "center",
-    justifyContent: "space-around"
+    justifyContent: "space-around",
+    marginBottom: 30
   }
 });
